79d4ebda7b366b1a9a6c0203189edef751b746f9
ankit.metcor@gmail.com <ankit.metcor@gmail.com@0a241e1f-f226-746e-11b6-a245570be289>
2013-12-21 06:56:14 +0000

    fully implemented and tested Compound, Pair classes

--- trunk/src/Compound.java
+++ trunk/src/Compound.java
@@ -1,5 +1,50 @@
 /**
  * Compound.java
  * Basic data structure for reference values (RKMD)
- * 
  * */
+
+package rkimp;
+ 
+public class Compound{
+	
+	private String name;
+	private double mObs;
+	private double refKMD;
+	private int nChains;
+	
+	public Compound(String auxName, int auxNChains, double auxMObs, double auxRefKMD){
+		this.name = auxName;
+		this.nChains = auxNChains;
+		this.mObs = auxMObs;
+		this.refKMD = auxRefKMD;
+	}
+	
+	public Compound(String auxName, int auxNChains, double auxRefKMD){
+		this.name = auxName;
+		this.nChains = auxNChains;
+		this.refKMD = auxRefKMD;
+		this.mObs = -1; //any mObs < 0 denotes that mObs has not been set
+	}
+	
+	//accessors:
+	
+	public boolean isSpecies(String auxKey){
+		return auxKey.equalsIgnoreCase(this.name);
+	}
+	
+	public double getRefKMD(){
+		return this.refKMD;
+	}
+	
+	public double getmObs(){
+		return this.mObs;
+	}
+	
+	public double getNC(){
+		return this.nChains;
+	}
+	
+	
+ 
+ 
+}
