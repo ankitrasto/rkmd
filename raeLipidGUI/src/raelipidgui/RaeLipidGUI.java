17cf0f16a7bf174435e76343d09b781a84186019
ankit.metcor <ankit.metcor@gmail.com>
2014-10-19 03:55:18 +0000

    GUI overhaul, backend minor fixes for output

---
+++ raeLipidGUI/src/raelipidgui/RaeLipidGUI.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package raelipidgui;
+
+/**
+ *
+ * @author ankit
+ */
+public class RaeLipidGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        raelipidgui.main app = new raelipidgui.main();
+        app.setVisible(true);
+    }
+    
+}
